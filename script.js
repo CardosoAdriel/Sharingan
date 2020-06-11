fcb2cda9b65b80187dd8c25d5a4b61282821703b
Adriel Cardoso <adrmcardoso@outlook.com>
2020-06-11 17:27:19 -0300

    adicionado efeito de som na animação

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ function show(){
   const eye = document.querySelector('.normal')
   const eye2 = document.querySelector('#eye')
   const fire = document.querySelector('#modal')
+  const audio = document.querySelector('#audio')
 
   eye.classList.toggle('show')
   eye2.classList.toggle('show')
+  audio.innerHTML = `
+      <audio autoplay>
+        <source src="./sound/amaterasu.mp3">
+      </audio>
+  `
+
 
   setTimeout(() => {
     fire.style.opacity = '1'
@@ -20,14 +27,10 @@ function show(){
         setTimeout(() => {
             eye.classList.toggle('show')
             eye2.classList.toggle('show')
-        }, 2000);
+        }, 1000);
 
-      }, 3000);
+      }, 2000);
 
-  }, 3000);
-
-
-
-
+  }, 1000);
 
 }
